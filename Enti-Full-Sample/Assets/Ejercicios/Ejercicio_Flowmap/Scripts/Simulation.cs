c633181fb3eeb0dfb954fc0f221949d2943e078c
CarlesPontacq <carles.pontacq@enti.cat>
2026-05-29 19:57:53 +0200

    Player position track completed

---
+++ Enti-Full-Sample/Assets/Ejercicios/Ejercicio_Flowmap/Scripts/Simulation.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Simulation : MonoBehaviour
+{
+    public Transform minSimulation;
+    public Transform maxSimulation;
+    public Transform player;
+    [FormerlySerializedAs("material")]
+    public Material simulationMaterial;
+    public RenderTexture renderTexture;
+
+    public Material instanceMaterial;
+
+    private Vector3 lastPosition;
+    private RenderTexture lastFrame;
+
+    private void Awake()
+    {
+        lastFrame = new RenderTexture(renderTexture.width, renderTexture.height, renderTexture.depth);
+
+        instanceMaterial = new Material(simulationMaterial);
+        Shader.SetGlobalVector("_MinPosSimulation", minSimulation.position);
+        Shader.SetGlobalVector("_MaxPosSimulation", maxSimulation.position);
+
+        lastPosition = player.position;
+    }
+
+    void Update()
+    {
+        var direction = player.position - lastPosition;
+        instanceMaterial.SetVector("_Direction", direction.normalized);
+        instanceMaterial.SetVector("_PlayerPosition", player.position);
+        instanceMaterial.SetTexture("_LastFrame", lastFrame);
+
+        Graphics.Blit(null, renderTexture, instanceMaterial);
+        Graphics.Blit(renderTexture, lastFrame);
+
+        lastPosition = player.position;
+    }
+}
